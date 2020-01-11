cd93e720e9014167d9c8304bb6a341499e15ab1e
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-11 21:21:38 +0200

    Add RegistrantPayment model

--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+from registration.models import Registrant
+
 
 class Payment(models.Model):
     amount = models.FloatField()
+
+
+class RegistrantPayment(models.Model):
+    registrant = models.ForeignKey(
+        to=Registrant,
+        related_name="payments",
+        on_delete=models.PROTECT,
+    )
+    payment = models.ForeignKey(
+        to=Payment,
+        related_name="registrants",
+        on_delete=models.PROTECT,
+    )
