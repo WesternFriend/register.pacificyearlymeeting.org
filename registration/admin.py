697ae34eca56a4ef19cd113a4de946bfd36faa7e
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-11 21:03:29 +0200

    Register registrant with model admin

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from registration.models import Registrant
+
+
+class RegistrantModelAdmin(ModelAdmin):
+    """Registrant model admin."""
+
+    model = Registrant
+    menu_label = "Registrants"
+    menu_icon = "fa-user"
+    menu_order = 100
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "full_name",
+        "registration_cost"
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+    )
+
+
+modeladmin_register(RegistrantModelAdmin)
