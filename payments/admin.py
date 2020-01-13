be88f855f4e211bfe935115d864cd254aad98785
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-13 18:48:09 +0200

    Add Payment and RegistrantPayment model admins

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,38 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from payments.models import Payment, RegistrantPayment
+
+
+class PaymentModelAdmin(ModelAdmin):
+    """Registrant model admin."""
+
+    model = Payment
+    menu_label = "Payments"
+    menu_icon = "tick"
+    menu_order = 110
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "amount",
+    )
+
+
+class RegistrantPaymentModelAdmin(ModelAdmin):
+    """Registrant model admin."""
+
+    model = RegistrantPayment
+    menu_label = "Registrant Payments"
+    menu_icon = "success"
+    menu_order = 120
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "amount",
+    )
+
+
+modeladmin_register(PaymentModelAdmin)
+modeladmin_register(RegistrantPaymentModelAdmin)
