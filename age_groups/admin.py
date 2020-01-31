172fc5d5e9318ec30c61db0066a7063a2f7eab58
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 17:06:10 +0200

    Initial AgeGroupModelAdmin

--- age_groups/admin.py
+++ age_groups/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from age_groups.models import AgeGroup
+
+
+class AgeGroupModelAdmin(ModelAdmin):
+    """Registrant model admin."""
+
+    model = AgeGroup
+    menu_label = "Age groups"
+    menu_icon = "fa-users"
+    menu_order = 102
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "name",
+    )
+
+
+modeladmin_register(AgeGroupModelAdmin)
