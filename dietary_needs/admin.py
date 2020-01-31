1b46e841bca7b05656a2c8b9f0406d6f98f4a8db
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 17:24:58 +0200

    Add DietaryNeedModelAdmin

--- dietary_needs/admin.py
+++ dietary_needs/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from dietary_needs.models import DietaryNeed
+
+
+class DietaryNeedModelAdmin(ModelAdmin):
+    """Manage age groups."""
+
+    model = DietaryNeed
+    menu_label = "Dietary needs"
+    menu_icon = "fa-cutlery"
+    menu_order = 102
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "name",
+    )
+
+
+modeladmin_register(DietaryNeedModelAdmin)
