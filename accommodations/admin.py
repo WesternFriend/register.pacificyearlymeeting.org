7e247aa8cf7b5353b7f0e1ae43ee39a6fed5dbe7
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 16:47:34 +0200

    Initial AccommodationModelAdmin

--- accommodations/admin.py
+++ accommodations/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from accommodations.models import Accommodation
+
+
+class AccommodationModelAdmin(ModelAdmin):
+    """Registrant model admin."""
+
+    model = Accommodation
+    menu_label = "Accommodations"
+    menu_icon = "fa-home"
+    menu_order = 102
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "name",
+    )
+
+
+modeladmin_register(AccommodationModelAdmin)
