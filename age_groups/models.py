ebbe841ffc2d15a9e6517498f6b3a196a824cdd8
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 17:05:47 +0200

    Initial AgeGroup model

--- age_groups/models.py
+++ age_groups/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class AgeGroup(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+
+    class Meta:
+        db_table = "age_group"
+
+    def __str__(self):
+        return self.name
