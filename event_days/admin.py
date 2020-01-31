6054819c25e7a588faecbb7d498d002920f8fa85
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 21:38:34 +0200

    Add event day model admin

--- event_days/admin.py
+++ event_days/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from event_days.models import EventDay
+
+
+class EventDayModelAdmin(ModelAdmin):
+    """Registrant model admin."""
+
+    model = EventDay
+    menu_label = "Event days"
+    menu_icon = "fa-calendar"
+    menu_order = 102
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "date",
+        "partial_day_discount",
+    )
+
+
+modeladmin_register(EventDayModelAdmin)
