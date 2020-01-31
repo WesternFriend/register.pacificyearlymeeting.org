f67cb4b104848a0c09cae52c4d742b3417baccdd
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 21:33:33 +0200

    Reset migrations

--- event_days/models.py
+++ event_days/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class EventDay(models.Model):
+    date = models.DateField(
+        help_text="Choose one of the event days",
+        unique=True
+    )
+    partial_day_discount = models.IntegerField(
+        null=True,
+        blank=True,
+        help_text="Enter the optional amount in USD that each day registration should be discounted on this date."
+    )
+
+    class Meta:
+        db_table = "event_day"
