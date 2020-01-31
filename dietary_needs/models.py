ab2036d715a7bca2bdc1b2815ca320ac6f69ee53
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-31 17:21:30 +0200

    Initial DietaryNeed model

--- dietary_needs/models.py
+++ dietary_needs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class DietaryNeed(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+
+    class Meta:
+        db_table = "dietary_need"
+
+    def __str__(self):
+        return self.name
