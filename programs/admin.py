58eb8e4b254f454d33b285ad603e5b5cd54e54be
Brylie Christopher Oxley <brylie@amble.fi>
2020-02-01 19:42:38 +0200

    Initial ProgramChoiceModelAdmin

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
 
-# Register your models here.
+from programs.models import ProgramChoice
+
+
+class ProgramChoiceModelAdmin(ModelAdmin):
+    """Manage program choices."""
+
+    model = ProgramChoice
+    menu_label = "Program choices"
+    menu_icon = "fa-users"
+    menu_order = 102
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "name",
+        "age_min",
+        "age_max",
+    )
+    list_filter = ("age_min", "age_max")
+
+
+modeladmin_register(ProgramChoiceModelAdmin)
