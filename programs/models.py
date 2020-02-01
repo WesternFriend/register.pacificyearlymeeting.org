fb824987396f32cb27c5e48e3f9943756911e34c
Brylie Christopher Oxley <brylie@amble.fi>
2020-02-01 19:36:07 +0200

    Initial ProgramChoice model

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,24 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class ProgramChoice(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+    age_min = models.IntegerField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    age_max = models.IntegerField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+
+    class Meta:
+        db_table = "program_choice"
+
+    def __str__(self):
+        return self.name
