bf2147c5b5d8bfca30f8aa98dd5f32586c81702e
Brylie Christopher Oxley <brylie@amble.fi>
2020-02-01 22:16:49 +0200

    Initial DayAttenderFee model

--- fees/models.py
+++ fees/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class DayAttenderFee(models.Model):
+    age_min = models.IntegerField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    age_max = models.IntegerField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    daily_fee = models.IntegerField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+
+    class Meta:
+        db_table = "day_attender_fee"
+
+    def __str__(self):
+        return f"Age: {self.age_min} to {self.age_max}. Daily fee: {self.daily_fee}"
